b650e10441368a30d92426e88c9f07e20a534e85
Cozynxis/Levi <levi.jvbcollege@gmail.com>
2026-08-28 17:10:25 +0200

    Fix update log trigger on install and update

--- Chromium/sw.js
+++ Chromium/sw.js
@@ -1,12 +1,16 @@
 // SERVICE WORKER
 chrome.runtime.onInstalled.addListener(({ reason }) => {
+    const version = chrome.runtime.getManifest().version;
+
     if (reason == chrome.runtime.OnInstalledReason.INSTALL) {
         chrome.runtime.openOptionsPage();
-        chrome.storage.local.set({
-            enabled: true,
-            somtoday_mod_update_log_pending: false,
-            somtoday_mod_update_log_pending_version: '',
-            somtoday_mod_update_log_last_shown: chrome.runtime.getManifest().version
+        chrome.storage.local.get(['somtoday_mod_update_log_disabled']).then((data) => {
+            chrome.storage.local.set({
+                enabled: true,
+                somtoday_mod_update_log_pending: data.somtoday_mod_update_log_disabled === true ? false : true,
+                somtoday_mod_update_log_pending_version: data.somtoday_mod_update_log_disabled === true ? '' : version,
+                somtoday_mod_update_log_last_shown: ''
+            });
         });
     }
 
@@ -15,7 +19,8 @@ chrome.runtime.onInstalled.addListener(({ reason }) => {
             if (data.somtoday_mod_update_log_disabled === true) return;
             chrome.storage.local.set({
                 somtoday_mod_update_log_pending: true,
-                somtoday_mod_update_log_pending_version: chrome.runtime.getManifest().version
+                somtoday_mod_update_log_pending_version: version,
+                somtoday_mod_update_log_last_shown: ''
             });
         });
     }
