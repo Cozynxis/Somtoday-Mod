740d51cb258d091192051ac63bb357c8a4bc5532
Cozynxis/Levi <levi.jvbcollege@gmail.com>
2026-08-28 17:04:06 +0200

    Trigger update log after extension update

--- Firefox/sw.js
+++ Firefox/sw.js
@@ -2,7 +2,22 @@
 chrome.runtime.onInstalled.addListener(({ reason }) => {
     if (reason == chrome.runtime.OnInstalledReason.INSTALL) {
         chrome.runtime.openOptionsPage();
-        chrome.storage.local.set({ enabled: true });
+        chrome.storage.local.set({
+            enabled: true,
+            somtoday_mod_update_log_pending: false,
+            somtoday_mod_update_log_pending_version: '',
+            somtoday_mod_update_log_last_shown: chrome.runtime.getManifest().version
+        });
+    }
+
+    if (reason == chrome.runtime.OnInstalledReason.UPDATE) {
+        chrome.storage.local.get(['somtoday_mod_update_log_disabled']).then((data) => {
+            if (data.somtoday_mod_update_log_disabled === true) return;
+            chrome.storage.local.set({
+                somtoday_mod_update_log_pending: true,
+                somtoday_mod_update_log_pending_version: chrome.runtime.getManifest().version
+            });
+        });
     }
 });
 chrome.runtime.setUninstallURL("https://jonazwetsloot.nl/somtoday-mod-bye");
