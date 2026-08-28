abff33610c92aa3fa4ea2d3218503be6d1d1ad7a
Cozynxis/Levi <levi.jvbcollege@gmail.com>
2026-08-28 17:14:05 +0200

    Remove update log install handling from Firefox

--- Firefox/sw.js
+++ Firefox/sw.js
@@ -1,28 +1,8 @@
 // SERVICE WORKER
 chrome.runtime.onInstalled.addListener(({ reason }) => {
-    const version = chrome.runtime.getManifest().version;
-
     if (reason == chrome.runtime.OnInstalledReason.INSTALL) {
         chrome.runtime.openOptionsPage();
-        chrome.storage.local.get(['somtoday_mod_update_log_disabled']).then((data) => {
-            chrome.storage.local.set({
-                enabled: true,
-                somtoday_mod_update_log_pending: data.somtoday_mod_update_log_disabled === true ? false : true,
-                somtoday_mod_update_log_pending_version: data.somtoday_mod_update_log_disabled === true ? '' : version,
-                somtoday_mod_update_log_last_shown: ''
-            });
-        });
-    }
-
-    if (reason == chrome.runtime.OnInstalledReason.UPDATE) {
-        chrome.storage.local.get(['somtoday_mod_update_log_disabled']).then((data) => {
-            if (data.somtoday_mod_update_log_disabled === true) return;
-            chrome.storage.local.set({
-                somtoday_mod_update_log_pending: true,
-                somtoday_mod_update_log_pending_version: version,
-                somtoday_mod_update_log_last_shown: ''
-            });
-        });
+        chrome.storage.local.set({ enabled: true });
     }
 });
 chrome.runtime.setUninstallURL("https://jonazwetsloot.nl/somtoday-mod-bye");
